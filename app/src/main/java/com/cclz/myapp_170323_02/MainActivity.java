65959213c79053fa3048d71337e843d7525d06d4
Cclz <rex620620>
2017-03-23 10:17:07 +0800

    setSingleChoiceItems 1

--- app/src/main/java/com/cclz/myapp_170323_02/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170323_02/MainActivity.java
@@ -45,6 +45,8 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("輸入測試");
         final TextView tv=(TextView)findViewById(R.id.textView); // 匿名物件需+ final
+        builder.setMessage("請輸入訊息:");
+        builder.setIcon(android.R.drawable.btn_plus);   // 系統內建圖檔
         final EditText ed=new EditText(MainActivity.this); // 匿名物件需+ final
         builder.setView(ed);
         builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
@@ -52,6 +54,32 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(DialogInterface dialog, int which) {
                 Log.d("DLG", "使用者按下確定");
                 tv.setText(ed.getText().toString());    // 匿名物件需+ final
+            }
+        });
+        builder.setNeutralButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下取消");
+            }
+        });
+        builder.show();
+    }
+
+    public void click3(View v){
+        AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("選項測試");
+        final String fruits[] = getResources().getStringArray(R.array.fruits);
+        builder.setSingleChoiceItems(R.array.fruits, -1, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                TextView tv2=(TextView)findViewById(R.id.textView2);
+                tv2.setText(fruits[which]);
+            }
+        });
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下確定");
             }
         });
         builder.setNeutralButton("取消", new DialogInterface.OnClickListener() {
