f03a529d6184368b2af51cca8307c1e86a89678f
Cclz <rex620620>
2017-03-23 09:21:49 +0800

    add 3rd button

--- app/src/main/java/com/cclz/myapp_170323_02/MainActivity.java
+++ app/src/main/java/com/cclz/myapp_170323_02/MainActivity.java
@@ -18,16 +18,22 @@ public class MainActivity extends AppCompatActivity {
         AlertDialog.Builder builder=new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("對話框測試");
         builder.setMessage("這是一個對話框測試");
-        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() {
+        builder.setPositiveButton("確定", new DialogInterface.OnClickListener() { // 確定按鈕
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Log.d("DLG", "使用者按下確定");
             }
         });
-        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() { // 取消按鈕
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Log.d("DLG", "使用者按下取消");
+            }
+        });
+        builder.setNeutralButton("看說明", new DialogInterface.OnClickListener() { // 中立按鈕
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Log.d("DLG", "使用者按下看說明");
             }
         });
         builder.show();
